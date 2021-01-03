695b692ad2875af9524ed1eff8d4c6c7b0fc0187
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2021-01-02 21:53:57 -0800

    added pawn and refactored code

--- chess.py
+++ chess.py
@@ -1,15 +1,22 @@
 import pygame
 import pathlib
 
+from pygame.constants import K_f, K_r
+
 pygame.init()
 
 #  Setting up the window
-screen_width = 480
-screen_height = 480
+screen_width = 600
+screen_height = 600
 mod = (screen_height + screen_width) // 16
 
 # Colors
-YELLOW = (225, 225, 0, 128)
+RED = (255, 0, 0, 50)
+GREEN = (0, 255, 0, 50)
+BLUE = (0, 0, 255, 50)
+WHITE = (255, 255, 255, 50)
+BLACK = (0, 0, 0, 50)
+YELLOW = (255, 255, 0, 50)
 TAN = (240, 217, 181, 128)
 DARK_TAN = (181, 136, 99, 128)
 
@@ -83,10 +90,10 @@ def pieceUnderMouse(board):
 def highlightRect(x, y):
     pygame.draw.rect(win, YELLOW, (y * mod, x * mod, mod, mod))
     if abs(x - y) % 2 == 0:
-        pygame.draw.rect(win, TAN, (y * mod + 5, x * mod + 5, mod - 10, mod - 10))
+        pygame.draw.rect(win, TAN, (y * mod + 3, x * mod + 3, mod - 6, mod - 6))
 
     else:
-        pygame.draw.rect(win, DARK_TAN, (y * mod + 5, x * mod + 5, mod - 10, mod - 10))
+        pygame.draw.rect(win, DARK_TAN, (y * mod + 3, x * mod + 3, mod - 6, mod - 6))
 
 
 def highlightValid(validlist):
@@ -95,6 +102,41 @@ def highlightValid(validlist):
 
     for i in range(len(validlist)):
         highlightRect(validlist[i][0], validlist[i][1])
+
+
+def restart(board):
+    return [[-4, -2, -3, -5, -6, -3, -2, -4],
+             [-1, -1, -1, -1, -1, -1, -1, -1],
+             [ 0,  0,  0,  0,  0,  0,  0,  0],
+             [ 0,  0,  0,  0,  0,  0,  0,  0],
+             [ 0,  0,  0,  3,  0,  0,  0,  0],
+             [ 0,  0,  0,  0,  0,  0,  0,  0],
+             [ 1,  1,  1,  1,  1,  1,  1,  1],
+             [ 4,  2,  3,  5,  6,  3,  2,  4]]
+
+
+def flipBoard(board):
+    newBoard = []
+    for i in range(len(board)):
+        newRow = []
+        for j in range(len(board[i])):
+            newRow.append(board[7 - i][7 - j])
+
+        newBoard.append(newRow)
+
+    return newBoard
+
+
+def createPiece(color, board, piece, x, y):
+    piece = abs(piece)
+    pieceMap = {1: Pawn(color, board, x, y),
+                2: Knight(color, board, x, y),
+                3: Bishop(color, board, x, y),
+                4: Rook(color, board, x, y),
+                5: Queen(color, board, x, y),
+                6: King(color, board, x, y)}
+
+    return pieceMap[piece]
 
 
 def main():
@@ -145,9 +187,17 @@ def main():
         x, y = getMousePos()    
 
         for event in pygame.event.get():
+            state = pygame.key.get_pressed()
+            if state[K_r]:
+                board = restart(board)
+
+            if state[K_f]:
+                board = flipBoard(board)
+                
+
             if event.type == pygame.QUIT:
                 running = False
-
+            
             # Checking if the mouse has been pressed
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if piece != 0 and not isClicked:
@@ -161,38 +211,23 @@ def main():
             # Checking if the mouse has been released
             if event.type == pygame.MOUSEBUTTONUP:
                 if isClicked:
+                    
                     board[curr_x][curr_y] = curr_piece
+                    print(curr_x, curr_y, x, y)
                     if [x, y] in valid:
                         
                         # Place the piece
                         Piece.placePiece(board, curr_x, curr_y, x, y)
-                    
 
+        
                 isClicked = False
                 
-                    
-
+                
         # If the mouse button is clicked
         if isClicked:
             board[curr_x][curr_y] = 0
             color = curr_piece // abs(curr_piece)
-            if abs(curr_piece) == 1:
-                curr = Pawn(color, board, curr_x, curr_y)
-
-            elif abs(curr_piece) == 2:
-                curr = Knight(color, board, curr_x, curr_y)
-
-            elif abs(curr_piece) == 3:
-                curr = Bishop(color, board, curr_x, curr_y)
-
-            elif abs(curr_piece) == 4:
-                curr = Rook(color, board, curr_x, curr_y)
-
-            elif abs(curr_piece) == 5:
-                curr = Queen(color, board, curr_x, curr_y)
-
-            elif abs(curr_piece) == 6:
-                curr = King(color, board, curr_x, curr_y)
+            curr = createPiece(color, board, curr_piece, curr_x, curr_y)
 
             valid = curr.validMoves()
             # Using drag n' drop functionality to move the piece
@@ -232,33 +267,41 @@ class Piece:
 
 
 class Pawn(Piece):
+
+    # Generates valid moves for a pawn
     def validMoves(self):
+        # Gets info about pawn at (x, y)
         color, board, x, y = Piece.getVar(self)
+
+        # Checking if its positioned at original spot/hasn't moved yet
         validList = []
-        for z in range(3):
+        if x == 1 or x == 6:
+            end = 3
+        
+        else:
+            end = 2
+
+        for z in range(1, end):
+
+            # Only checking for vertical moves
             if color > 0:
                 new_x = x - z
-                new_y = y - z
             
             else:
                 new_x = x + z
-                new_y = y + z
 
-            if 0 <= new_x <= 7:
+            if 0 <= new_x <= 7 and board[new_x][y] == 0:
                 validList.append([new_x, y])
-
-                # Pawn capture functionality
-                if board[new_x][new_y] != 0:
-                    validList.append([new_x, new_y])
-
-                if board[new_x][new_y] != 0:
-                    validList.append([new_x, new_y])
-
 
             else:
                 break
         
-        
+        # Pawn capture functionality(checks left and right positions)
+        if board[x - color][y - 1] * color < 0:
+            validList.append([x - color, y - 1])
+
+        if board[x - color][y + 1] * color < 0:
+            validList.append([x - color, y + 1])
 
         return validList
 
@@ -267,18 +310,7 @@ class Knight(Piece):
     def validMoves(self):
         color, board, x, y = Piece.getVar(self)
         validList = []
-        for z in range(3):
-            if color > 0:
-                new_x = x - z
-            
-            else:
-                new_x = x + z
-
-            if 0 <= new_x <= 7:
-                validList.append([new_x, y])
-
-            else:
-                break
+        
 
         return validList
 
@@ -290,7 +322,8 @@ class Bishop(Piece):
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if abs(i - x) == abs(j - y):
-                    validList.append([i, j])
+                    if [i, j] != [x, y]:
+                        validList.append([i, j])
 
         return validList
 
@@ -302,7 +335,8 @@ class Rook(Piece):
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if i == x or j == y:
-                    validList.append([i, j])
+                    if [i, j] != [x, y]:
+                        validList.append([i, j])
 
         return validList
 
@@ -310,13 +344,10 @@ class Rook(Piece):
 class Queen(Piece):
     def validMoves(self):
         color, board, x, y = Piece.getVar(self)
-        validList = []
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if (abs(i - x) == abs(j - y)) or (i == x or j == y):
-                    validList.append([i, j])
+        b = Bishop(color, board, x, y)
+        r = Rook(color, board, x, y)
 
-        return validList
+        return b.validMoves() + r.validMoves()
 
 
 class King(Piece):
@@ -326,8 +357,10 @@ class King(Piece):
         cor1, cor2 = x - 1, y - 1
         for i in range(3):
             for j in range(3):
-                if 0 <= cor1 <= 7 and 0 <= cor2 <= 7:
-                    validList.append([cor1, cor2])
+                if 0 <= cor1 <= 7 and 0 <= cor2 <= 7: 
+                    if board[cor1][cor2] * color <= 0:
+                        if [cor1, cor2] != [x, y]:
+                            validList.append([cor1, cor2])
 
                 cor2 += 1
 
